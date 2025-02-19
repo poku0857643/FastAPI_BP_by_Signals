d612fe36b4f3603d9428330b58b124b7e902395f
lucy <lucy199043@gmail.com>
2025-02-19 21:48:59 +0800

    build real-time component to combine manual data update in dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -6,20 +6,25 @@ import httpx
 import asyncio
 from fastapi.responses import JSONResponse
 
-sbp_prediction_list = [120.0, 121.5, 135.0, 140.2]
-dbp_prediction_list = [80.0, 81.0, 85.0, 88.0]
+sbp_test_list = [120.0, 121.5, 135.0, 140.2]
+dbp_test_list = [80.0, 81.0, 85.0, 88.0]
 
+sbp_prediction_list = []
+dbp_prediction_list = []
 
 # prediction visualizer
 dash_app = dash.Dash(__name__)
-dash_app.title = "Blood Pressure Prediction Visualizer"
-
-
+dash_app.title = "Blood Pressure Real-Time Prediction Visualizer"
 
 # html layout of visualizer
 dash_app.layout =html.Div([
     html.H1("Blood Pressure Prediction Visualizer", style = {'textAlign': 'center'}),
     dcc.Graph(id='bp-visualization'),
+    dcc.Interval(
+        id='interval-component', # timer component
+        interval = 5000, # 5secs
+        n_intervals = 0 # init = 0
+    )
     html.Button('Update Data', id='update-btn', n_clicks=0, style={'margin': '20px'})
 ])
 
@@ -35,10 +40,13 @@ async def fetch_predictions():
 
 @dash_app.callback(
     Output('bp-visualization', 'figure'),
-    Input('update-btn', 'n_clicks')
+    Input('update-btn', 'n_clicks'),
+    [Input('interval-component', 'n_intervals')]
 )
 
-def update_graph(n_clicks, sbp_test_list = sbp_prediction_list, dbp_test_list = dbp_prediction_list):
+def update_graph(n_intervals, n_clicks: int = 0):
+    global sbp_prediction_list, dbp_prediction_list
+
     if n_clicks == 0:
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=[0], y=sbp_test_list, mode="lines+markers", name="SBP"))
@@ -48,7 +56,13 @@ def update_graph(n_clicks, sbp_test_list = sbp_prediction_list, dbp_test_list = 
         return fig
 
     try:
-        sbp_prediction_list, dbp_prediction_list = asyncio.run(fetch_predictions())
+        sbp_data, dbp_data = asyncio.run(fetch_predictions())
+        sbp_prediction_list += sbp_data
+        dbp_prediction_list += dbp_data
+
+        sbp_prediction_list = sbp_prediction_list[-50:]
+        dbp_prediction_list = dbp_prediction_list[-50:]
+
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=list(range(len(sbp_prediction_list))), y=sbp_prediction_list, mode="lines+markers", name="SBP"))
         fig.add_trace(go.Scatter(x=list(range(len(dbp_prediction_list))), y=dbp_prediction_list, mode="lines+markers", name="DBP"))
@@ -56,6 +70,7 @@ def update_graph(n_clicks, sbp_test_list = sbp_prediction_list, dbp_test_list = 
         return fig
 
     except Exception as e:
+        print("Error fetching data:", e)
         return go.Figure()
 
 if __name__ == "__main__":
